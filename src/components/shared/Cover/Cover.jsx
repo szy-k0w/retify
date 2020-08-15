ec9c82b095399b6e92ba9a21ec9e272429232458
Sz7Kow <szymon.kowalski2000@gmail.com>
2020-08-15 17:25:06 +0200

    Create Cover component

---
+++ src/components/shared/Cover/Cover.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Cover.sass";
+
+const Cover = ({ src, alt }) => {
+	return (
+		<div className="cover">
+			<main className="cover__content">
+				<img className="cover__img" src={src} alt={alt} />
+			</main>
+		</div>
+	);
+};
+
+Cover.propTypes = {
+	src: PropTypes.string.isRequired,
+	alt: PropTypes.string.isRequired,
+};
+
+Cover.defaultProps = {
+	alt: "Cover",
+};
+
+export default Cover;
