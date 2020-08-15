f932585a0629a27803b2d128f11f7b1d6bb3bad0
Sz7Kow <szymon.kowalski2000@gmail.com>
2020-08-15 10:19:18 +0200

    Create App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router";
 
 import "./App.sass";
 
 const App = (props) => {
-	return <div className="app">App</div>;
+	return (
+		<div className="app">
+			<Switch>
+				<Route path="/auth">{/* TODO: ADD SUBPAGE */}</Route>
+				<Route path="/callback">{/* TODO: ADD SUBPAGE */}</Route>
+				<Route path="/client">{/* TODO: ADD SUBPAGE */}</Route>
+			</Switch>
+		</div>
+	);
 };
 
 export default App;
