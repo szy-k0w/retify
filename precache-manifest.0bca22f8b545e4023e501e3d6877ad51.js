0694c86a43c3425b3a1c55afd7e0185a792d0a01
Sz7Kow <szymon.kowalski2000@gmail.com>
2020-08-26 03:13:03 +0200

    Updates

---
+++ precache-manifest.0bca22f8b545e4023e501e3d6877ad51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da74e6d28a1dfa5cc8c40db73a00125",
+    "url": "/retify/index.html"
+  },
+  {
+    "revision": "3ad9a08f6cead9847d54",
+    "url": "/retify/static/css/main.787a3871.chunk.css"
+  },
+  {
+    "revision": "6b2d38d5b76c67c56af5",
+    "url": "/retify/static/js/2.30499678.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/retify/static/js/2.30499678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad9a08f6cead9847d54",
+    "url": "/retify/static/js/main.294ce64f.chunk.js"
+  },
+  {
+    "revision": "64436c11bf29867c9e7c",
+    "url": "/retify/static/js/runtime-main.d39fad55.js"
+  }
+]);
